03b2ba6d92da39fd5cf2ae92b0cca0926350bdd5
Asil Mian <a.mian@student.unimelb.edu.au>
2020-04-16 19:36:53 +1000

    first new canvas UI:

---
+++ Project 2/Assets/Scenes/Opening/OpenLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenLevel : MonoBehaviour
+{
+
+    public void getLevel(GameObject button)
+    {
+        if (button.name.Equals("Tutorial"))
+            SceneManager.LoadScene("Level1");
+        else if (button.name.Equals("Level1"))
+            SceneManager.LoadScene("Level2");
+        else if (button.name.Equals("Level2"))
+            SceneManager.LoadScene("Level3");
+    }
+}
